1b18abd50c3e8b02d3b3b44e74307bfdf0349dd6
EndlessCheng <loli.con@qq.com>
2014-07-10 15:59:28 +0800

    实现子类及Demo

---
+++ src/StrategyPattern/Demo.java
@@ -0,0 +1,11 @@
+package StrategyPattern;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		 Duck mallard=new MallardDuck();
+		 mallard.performQuack();
+		 mallard.performFly();
+	}
+
+}
